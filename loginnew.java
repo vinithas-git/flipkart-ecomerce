e7b9e77296b4c51b63874586e7a428dc9b54ad03
vinitha <sathyaseelan.vinitha@gmail.com>
2021-04-26 01:24:19 +0530

    loginnew.java added in master repository

---
+++ loginnew.java
@@ -0,0 +1,19 @@
+    public class Use_For_Loop
+
+    {
+
+        public static void main(String[] args)
+
+        {
+
+            for(int i = 1; i <= 20; i++)
+
+            {
+
+                System.out.println(i);
+
+            }
+
+        }
+
+    }
